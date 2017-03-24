ceef4ce43f64e1013afdbcce08c4f9d3b447dc5f
zhangjifeng <zhangjifeng@le.com>
2017-03-24 14:10:13 +0800

    抽签动画，旋转

--- app/src/main/java/com/letv/testanimation/MainActivity.java
+++ app/src/main/java/com/letv/testanimation/MainActivity.java
@@ -1,10 +1,12 @@
 package com.letv.testanimation;
 
+import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
@@ -17,13 +19,20 @@ import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import io.codetail.animation.ArcAnimator;
+import io.codetail.animation.Side;
 
 public class MainActivity extends Activity {
   private ImageView img;
+  private ImageView img_qian;
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     img = (ImageView) findViewById(R.id.img);
+    img_qian = (ImageView) findViewById(R.id.img_qian);
+    //img_qian.setPivotX(img_qian.getWidth()/2);
+    //img_qian.setPivotY(img_qian.getHeight()/2);
   }
 
   private void rotateAnimation() {
@@ -44,7 +53,6 @@ public class MainActivity extends Activity {
     img.startAnimation(animationSet);
 
   }
-  TranslateAnimation[] animations = new TranslateAnimation[2];
   private void translateAnimation() {
     TranslateAnimation translateAnimation = new TranslateAnimation(0,0,0,40);
     translateAnimation.setDuration(500);
@@ -52,7 +60,6 @@ public class MainActivity extends Activity {
     translateAnimation.setRepeatMode(Animation.REVERSE);
     translateAnimation.setRepeatCount(5);
     translateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
-    //animations[0] = translateAnimation;
 
     img.startAnimation(translateAnimation);
   }
@@ -74,9 +81,74 @@ public class MainActivity extends Activity {
     animatorSet.start();
   }
 
+  private void rotation() {
+    //ObjectAnimator moveX = ObjectAnimator.ofFloat(img_qian, "translationX", 0, 200 );
+    //ObjectAnimator moveY = ObjectAnimator.ofFloat(img_qian, "translationY", 0, 80 );
+    ObjectAnimator rotation = ObjectAnimator.ofFloat(img_qian, "rotation",  0, 15);
+    ObjectAnimator scaleAnimator1 = ObjectAnimator.ofFloat(img_qian, "scaleX", 1f, 0.25f);
+    ObjectAnimator scaleAnimator2 = ObjectAnimator.ofFloat(img_qian, "scaleY", 1f, 0.25f);
+    AnimatorSet as = new AnimatorSet();
+    as.setDuration(1);
+    as.playTogether(rotation, scaleAnimator1, scaleAnimator2);
+    as.addListener(new Animator.AnimatorListener() {
+      @Override public void onAnimationStart(Animator animation) {
+
+      }
+
+      @Override public void onAnimationEnd(Animator animation) {
+        img_qian.setVisibility(View.VISIBLE);
+        move();
+      }
+
+      @Override public void onAnimationCancel(Animator animation) {
+
+      }
+
+      @Override public void onAnimationRepeat(Animator animation) {
+
+      }
+    });
+    as.start();
+  }
+  private void move() {
+    //img_qian.setVisibility(View.VISIBLE);
+    int duration = 1000;
+    ArcAnimator.createArcShift(img_qian, 400, 450, 90, Side.LEFT).setDuration(duration).start();
+    ObjectAnimator scaleAnimator1 = ObjectAnimator.ofFloat(img_qian, "scaleX", 0.25f, 1f).setDuration(duration);
+    ObjectAnimator scaleAnimator2 = ObjectAnimator.ofFloat(img_qian, "scaleY", 0.25f, 1f).setDuration(duration);
+    ObjectAnimator rotation = ObjectAnimator.ofFloat(img_qian, "rotation",  15, 0).setDuration(duration);
+    ObjectAnimator rotationY = ObjectAnimator.ofFloat(img_qian,"rotationY",0,180, 360).setDuration(duration);
+    rotationY.addListener(
+        new Animator.AnimatorListener() {
+          @Override public void onAnimationStart(Animator animation) {
+
+          }
+
+          @Override public void onAnimationEnd(Animator animation) {
+            img_qian.setImageResource(R.drawable.qian);
+          }
+
+          @Override public void onAnimationCancel(Animator animation) {
+
+          }
+
+          @Override public void onAnimationRepeat(Animator animation) {
+
+          }
+        });
+
+    AnimatorSet set = new AnimatorSet();
+    set.setInterpolator(new AccelerateDecelerateInterpolator());
+    set.playTogether(scaleAnimator1, scaleAnimator2, rotation, rotationY);
+    set.start();
+
+  }
+
   @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
     if (keyCode==KeyEvent.KEYCODE_DPAD_CENTER) {
       translateAnimation();
+    } else if (keyCode==KeyEvent.KEYCODE_DPAD_DOWN) {
+      rotation();
     }
     return super.onKeyDown(keyCode, event);
   }
