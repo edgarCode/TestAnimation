ebf41cd883427a4902a9a876c7748dc0f7b2c661
zhangjifeng <zhangjifeng@le.com>
2017-03-24 16:29:32 +0800

    代码优化

--- app/src/main/java/com/letv/testanimation/MainActivity.java
+++ app/src/main/java/com/letv/testanimation/MainActivity.java
@@ -4,137 +4,115 @@ import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnticipateInterpolator;
-import android.view.animation.AnticipateOvershootInterpolator;
-import android.view.animation.BounceInterpolator;
-import android.view.animation.DecelerateInterpolator;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import com.bumptech.glide.Glide;
 import io.codetail.animation.ArcAnimator;
 import io.codetail.animation.Side;
 
 public class MainActivity extends Activity {
-  private ImageView img;
-  private ImageView img_qian;
-  private RelativeLayout layout_qiangpin;
-  private ImageView img_dialog_bg;
+  private ImageView image_mascot;//吉祥物
+  private ImageView image_stick, image_stick_backup;
+  private RelativeLayout layout_dialog_result;
+  private ImageView image_bg_dialog;
+
+  private final float scale_start = 0.25f;
+  private final float scale_end = 1.0f;
+  private final int arc_move = 90;
+  private final int arc_rotation = 15;
+  private int offsetX_dialog_bg;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    offsetX_dialog_bg = getResources().getDimensionPixelOffset(R.dimen.dp_700);
+
     setContentView(R.layout.activity_main);
-    img = (ImageView) findViewById(R.id.img);
-    img_qian = (ImageView) findViewById(R.id.img_qian);
-    layout_qiangpin = (RelativeLayout) findViewById(R.id.layout_qiangpin);
-    img_dialog_bg = (ImageView) findViewById(R.id.img_bg);
-  }
-
-  private void rotateAnimation() {
-    RotateAnimation rotateAnimation = new RotateAnimation(0, 30, Animation.RELATIVE_TO_SELF, 0.4f, Animation.RELATIVE_TO_SELF, 1.0f);
-    rotateAnimation.setDuration(1500);
-    rotateAnimation.setRepeatCount(1);
-    rotateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
-    rotateAnimation.setRepeatMode(Animation.REVERSE);
-    RotateAnimation rotateAnimation2 = new RotateAnimation(0, -30, Animation.RELATIVE_TO_SELF, 0.4f, Animation.RELATIVE_TO_SELF, 1.0f);
-    rotateAnimation2.setDuration(1500);
-    rotateAnimation2.setRepeatCount(1);
-    rotateAnimation2.setInterpolator(new AccelerateDecelerateInterpolator ());
-    rotateAnimation2.setRepeatMode(Animation.REVERSE);
-
-    AnimationSet animationSet = new AnimationSet(true);
-    animationSet.addAnimation(rotateAnimation);
-    animationSet.addAnimation(rotateAnimation2);
-    img.startAnimation(animationSet);
-
+    image_mascot = (ImageView) findViewById(R.id.img_mascot);
+    image_stick = (ImageView) findViewById(R.id.img_stick);
+    image_stick_backup = (ImageView) findViewById(R.id.img_stick_backup);
+    layout_dialog_result = (RelativeLayout) findViewById(R.id.layout_dialog_result);
+    image_bg_dialog = (ImageView) findViewById(R.id.img_dialog_bg);
   }
 
   private void rotation() {
-    ObjectAnimator rotation = ObjectAnimator.ofFloat(img_qian, "rotation",  0, 15);
-    ObjectAnimator scaleAnimator1 = ObjectAnimator.ofFloat(img_qian, "scaleX", 1f, 0.25f);
-    ObjectAnimator scaleAnimator2 = ObjectAnimator.ofFloat(img_qian, "scaleY", 1f, 0.25f);
+    ObjectAnimator rotation = ObjectAnimator.ofFloat(image_stick, "rotation",  0, arc_rotation);
+    ObjectAnimator scaleAnimator1 = ObjectAnimator.ofFloat(image_stick, "scaleX", scale_end, scale_start);
+    ObjectAnimator scaleAnimator2 = ObjectAnimator.ofFloat(image_stick, "scaleY", scale_end, scale_start);
     AnimatorSet as = new AnimatorSet();
     as.setDuration(1);
     as.playTogether(rotation, scaleAnimator1, scaleAnimator2);
-    as.addListener(new Animator.AnimatorListener() {
-      @Override public void onAnimationStart(Animator animation) {
-
-      }
-
+    as.addListener(new MyAnimationListener() {
       @Override public void onAnimationEnd(Animator animation) {
-        img_qian.setVisibility(View.VISIBLE);
+        image_stick.setVisibility(View.VISIBLE);
         move();
-      }
-
-      @Override public void onAnimationCancel(Animator animation) {
-
-      }
-
-      @Override public void onAnimationRepeat(Animator animation) {
-
       }
     });
     as.start();
   }
+  float offsetX, offsetY;
   private void move() {
-    int duration = 1300;
-    ArcAnimator.createArcShift(img_qian, 470, 530, 90, Side.LEFT).setDuration(duration).start();
-    ObjectAnimator scaleAnimator1 = ObjectAnimator.ofFloat(img_qian, "scaleX", 0.25f, 1f).setDuration(duration);
-    ObjectAnimator scaleAnimator2 = ObjectAnimator.ofFloat(img_qian, "scaleY", 0.25f, 1f).setDuration(duration);
-    ObjectAnimator rotation = ObjectAnimator.ofFloat(img_qian, "rotation",  15, 0).setDuration(duration);
-    scaleAnimator2.addListener(new Animator.AnimatorListener() {
-      @Override public void onAnimationStart(Animator animation) {
-
-      }
-
+    int duration_move = 1300;
+    int duration_rotationY = 800;
+    final int duration_show_bg = 1200;
+    DisplayMetrics dm = new DisplayMetrics();
+    ((WindowManager) (getSystemService(Context.WINDOW_SERVICE))).getDefaultDisplay().getMetrics(dm);
+    offsetX = image_stick.getX() - (dm.widthPixels- offsetX_dialog_bg)/2;
+    offsetY = image_stick.getY() - dm.heightPixels/2;
+    //弧度移动动画
+    ArcAnimator.createArcShift(image_stick, (dm.widthPixels- offsetX_dialog_bg)/2, dm.heightPixels/2, arc_move, Side.LEFT).setDuration(duration_move).start();
+    //横向缩放
+    ObjectAnimator scaleAnimator1 = ObjectAnimator.ofFloat(image_stick, "scaleX", scale_start, scale_end).setDuration(duration_move);
+    //纵向缩放
+    ObjectAnimator scaleAnimator2 = ObjectAnimator.ofFloat(image_stick, "scaleY", scale_start, scale_end).setDuration(duration_move);
+    //角度旋转为0
+    ObjectAnimator rotation = ObjectAnimator.ofFloat(image_stick, "rotation",  arc_rotation, 0).setDuration(duration_move);
+    scaleAnimator2.addListener(new MyAnimationListener() {
       @Override public void onAnimationEnd(Animator animation) {
-        layout_qiangpin.setVisibility(View.VISIBLE);
-        img_dialog_bg.animate().translationXBy(MainActivity.this.getResources().getDimensionPixelOffset(R.dimen.dp_700)).setDuration(1200).start();
-      }
-
-      @Override public void onAnimationCancel(Animator animation) {
-
-      }
-
-      @Override public void onAnimationRepeat(Animator animation) {
-
+        layout_dialog_result.setVisibility(View.VISIBLE);
+        //背景图动画展现
+        image_bg_dialog.animate().translationXBy(offsetX_dialog_bg).setDuration(duration_show_bg).start();
       }
     });
-    ObjectAnimator rotationY = ObjectAnimator.ofFloat(img_qian,"rotationY",0,180, 360).setDuration(800);
+    //翻转动画
+    ObjectAnimator rotationY = ObjectAnimator.ofFloat(image_stick,"rotationY",0, 180, 360).setDuration(duration_rotationY);
     rotationY.addListener(
-        new Animator.AnimatorListener() {
-          @Override public void onAnimationStart(Animator animation) {}
-
+        new MyAnimationListener() {
           @Override public void onAnimationEnd(Animator animation) {
-            img_qian.setImageResource(R.drawable.qian);
+            image_stick.setImageResource(R.drawable.stick_positive);
           }
-
-          @Override public void onAnimationCancel(Animator animation) {}
-
-          @Override public void onAnimationRepeat(Animator animation) {}
         });
 
     AnimatorSet set = new AnimatorSet();
     set.setInterpolator(new AccelerateDecelerateInterpolator());
     set.playTogether(scaleAnimator1, scaleAnimator2, rotation, rotationY);
     set.start();
+  }
 
+  private void reset() {
+      layout_dialog_result.setVisibility(View.GONE);
+      image_stick.setVisibility(View.INVISIBLE);
+      //重置签的背景
+      image_stick.setImageResource(R.drawable.stick_back);
+      //重置dialog背景图的位置
+      image_bg_dialog.animate().translationXBy(-offsetX_dialog_bg).setDuration(1).start();
+      //重置签的位置
+      ArcAnimator.createArcShift(image_stick, image_stick_backup, arc_move, Side.LEFT).setDuration(1).start();
   }
 
   @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
     if (keyCode==KeyEvent.KEYCODE_DPAD_CENTER) {
-      //translateAnimation();
-      MyAnimationDrawable.animateRawManuallyFromXML(R.drawable.xiaoren_animation, img, null, null);
+      MyAnimationDrawable.animateRawManuallyFromXML(R.drawable.xiaoren_animation, image_mascot, null, null);
     } else if (keyCode==KeyEvent.KEYCODE_DPAD_DOWN) {
       rotation();
+    } else if (keyCode==KeyEvent.KEYCODE_DPAD_UP) {
+      reset();
     }
     return super.onKeyDown(keyCode, event);
   }
---
+++ app/src/main/java/com/letv/testanimation/MyAnimationListener.java
@@ -0,0 +1,21 @@
+package com.letv.testanimation;
+
+import android.animation.Animator;
+
+/**
+ * Created by zhangjifeng on 2017/3/24.
+ */
+
+public abstract class MyAnimationListener implements Animator.AnimatorListener {
+  @Override public void onAnimationStart(Animator animation) {
+
+  }
+
+  @Override public void onAnimationCancel(Animator animation) {
+
+  }
+
+  @Override public void onAnimationRepeat(Animator animation) {
+
+  }
+}
